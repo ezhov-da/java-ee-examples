8a008111b674b2bf0a8e14e5c613091057560322
ezhov-da <ezhdenis@yandex.ru>
2017-10-30 05:24:24 +0300

    Joined table

---
+++ cdi-book-service/src/main/java/ru/ezhov/persistence/inheritance/joined/Book.java
@@ -0,0 +1,20 @@
+package ru.ezhov.persistence.inheritance.joined;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by rrnezh on 29.10.2017.
+ */
+@Entity
+public class Book extends Item {
+    private String author;
+
+    public Book(String author) {
+        this.author = author;
+    }
+
+    public Book(String name, String author) {
+        super(name);
+        this.author = author;
+    }
+}
